535b4519c789af08c4be765112bf3e2e041ffaec
Ross Donaldson <gastove@gmail.com>
2015-02-27 15:40:02 -0800

    more project deps!

--- project.clj
+++ project.clj
@@ -3,7 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [markdown-clj "0.9.63"]
+                 [me.raynes/fs "1.4.6"]]
   :main ^:skip-aot doctopus.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
