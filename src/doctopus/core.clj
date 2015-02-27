1a0975f9bd7dd09c11a4ff6109962b55d2f89023
Ross Donaldson <gastove@gmail.com>
2015-02-27 15:40:10 -0800

    notes that compile but dont run!

--- src/doctopus/core.clj
+++ src/doctopus/core.clj
@@ -5,3 +5,25 @@
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+
+(defn parse-a-doc
+  [some-local-path some-function some-output-path]
+  (some-function some-local-path some-output-path))
+
+(defn parse-markdown
+  [args]
+  (some-clojure-markdown-lib args))
+
+(defn parse-rst
+  [args]
+  (sh/exec-command "make html" args))
+
+(def a-configuration {:uri "/home/gastove/foo/my-hella-sweet-repo"
+                      :output-path "/who/knows"
+                      :documentation-type :markdown})
+
+(defmulti parse-documentation :documentation-type)
+
+(defmethod parse-documentation :markdown
+  [type & args])
