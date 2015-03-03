2690dc3212b069422639e80e50b0735818cc33f8
Ross Donaldson <gastove@gmail.com>
2015-03-02 17:34:54 -0800

    tweaking!

--- src/doctopus/files.clj
+++ src/doctopus/files.clj
@@ -17,16 +17,16 @@
 (defn filter-the-docs
   "Vector Triple of (path, directories, files), and function that filters relevant doc-types"
   [[path directories files] doc-type-fn]
+  (println directories)
   (if-let [matched-files (filter doc-type-fn files)]
     [path matched-files]
-    nil
-    ))
+    nil))
 
 
 (declare truncate-str)
 (defn assemble-the-results
   "More stuff"
-  [doc-root path matched-files]
+  [doc-root [path matched-files]]
   (let [str-path (.getPath path)
         str-rel-path (truncate-str doc-root str-path)
         ]
@@ -45,8 +45,8 @@
   "Walk the docs"
   [doc-root]
   (let [docs-all (fs/walk vector doc-root) ;; Triple of directory path, directory name, file name
-        filtered-docs (filter-the-docs docs-all)
-        assembled-result (assemble-the-results filtered-docs)
+        filtered-docs (filter-the-docs docs-all markdown-type)
+        assembled-result (assemble-the-results doc-root filtered-docs)
         ]
     assembled-result
     ))
