cc6a7f6904e490db94f9907433938d42c33d2dcc
Ross Donaldson <gastove@gmail.com>
2015-03-24 16:36:53 -0700

    Add our first test oh boy!

---
+++ test/doctopus/files_test.clj
@@ -0,0 +1,10 @@
+(ns doctopus.files-test
+  (:require [doctopus.files :refer :all]
+            [clojure.test :refer :all]))
+
+(deftest test-truncate-str
+  (testing "Can we truncate one path by removing another path from the front?"
+    (let [fq "/foo/bar/baz/bing/bang/document.markdown"
+          remove-str "/foo/bar/baz/bing/"
+          result (truncate-str fq remove-str)]
+      (is (= result "bang/document.markdown")))))
