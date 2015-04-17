529a7233b2e7be7a06550459301a38ab283f3110
Ross Donaldson <gastove@gmail.com>
2015-04-17 10:59:42 -0700

    We probably don't need any of this; right now it's just stopping master from compiling/tests passing. Let's revisit what we need here.

--- test/doctopus/core_test.clj
+++ test/doctopus/core_test.clj
@@ -2,6 +2,4 @@
   (:require [clojure.test :refer :all]
             [doctopus.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+;; I'm leaving this NS for now, but at the moment there's nothing real to test.
