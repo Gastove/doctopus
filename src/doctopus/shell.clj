3aa7e1bddb44746c70723fcad13702a767471610
Gavin McQuillan <gavin@urbanairship.com>
2015-04-22 13:28:43 -0700

    Fixes shelling out; testing!

--- src/doctopus/shell.clj
+++ src/doctopus/shell.clj
@@ -1,6 +1,5 @@
 (ns doctopus.shell
-    (:require [clojure.java.shell/sh :as sh]))
-
+    (:require [clojure.java.shell :as sh]))
 
 (defn make-html
 "This is a function that we use to shell out to a configured command
@@ -8,12 +7,14 @@ to build the HTML for a given project.
 
 e.g. cd /to/dir; make html
 
-TODO(gavin): what to do if output-dir is different from what we expect? 
-
 Args:
     command: string, something that is within the $PATH of a shell.
     args: a list of strings.
     root-dir: string, path to the directory where the ``command`` should be run.
 "
-[command args root-dir]
-    (apply sh (cons command args) :dir root-dir))
+    ([command]
+        (sh/sh command))
+    ([command args]
+        (apply sh/sh (cons command args)))
+    ([command args root-dir]
+        (apply sh/sh (cons command (conj args :dir root-dir)))))
