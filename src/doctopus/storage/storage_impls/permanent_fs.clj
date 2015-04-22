20dbb887ce2a657d756507b8693806fbfd4ddc77
Ross Donaldson <gastove@gmail.com>
2015-04-22 14:46:05 -0700

    Refactor a bunch of common pieces

--- src/doctopus/storage/storage_impls/permanent_fs.clj
+++ src/doctopus/storage/storage_impls/permanent_fs.clj
@@ -1,35 +1,16 @@
 (ns doctopus.storage.storage-impls.permanent-fs
-  (:require [clojure.string :as str]
-            [doctopus.files :as files]
-            [doctopus.files.storage-impls :refer :all]
+  (:require [doctopus
+             [configuration :refer [server-config]]
+             [files :as files]]
             [me.raynes.fs :as fs]
-            [doctopus.configuration :refer [server-config]])
-  (:import [doctopus.files.storage-impls.BackendImplementation]))
+            [doctopus.storage.storage-impls.fs-impl :refer [save-html-file]]))
 
 (def root (fs/file (:permanent-fs-root (server-config))))
 (if-not (fs/exists? root) (fs/mkdirs root))
 
-(defn- load-html-file
-  [rel-path]
-  (let [html-re #"\.html$"
-        assure-suffix #(if (re-find html-re rel-path) rel-path (str rel-path ".html"))
-        file-handle (fs/file root rel-path)]
-    (slurp file-handle)))
-
-(defn save-html-file
-  "Expects a key, a relative path, and the data to write. `Key' is
-  assumed to be the name of a particular documentation set, and
-  becomes the primary subdirectory in the filesystem."
-  [key rel-path data]
-  (binding [fs/*cwd* root]
-    (let [assure-relativity #(if (re-find #"^/" %) (subs % 1) %)
-          file-handle (fs/file key (assure-relativity rel-path))]
-      (fs/mkdirs (fs/parent file-handle))
-      (spit file-handle data))))
-
 (defn save-fn
   [key path-html-pairs]
-  (doseq [[path html] path-html-pairs] (save-html-file key path html)))
+  (doseq [[path html] path-html-pairs] (save-html-file root key path html)))
 
 (defn load-fn
   "This is as Version 1 a way of doing this as I can think of:
--- src/doctopus/storage/storage_impls/temp_fs.clj
+++ src/doctopus/storage/storage_impls/temp_fs.clj
@@ -2,6 +2,7 @@
   (:require [clojure.string :as str]
             [doctopus.files :as files]
             [doctopus.files.storage-impls :refer :all]
+            [doctopus.storage.storage-impls.fs-impl :refer [save-html-file]]
             [me.raynes.fs :as fs])
   (:import [doctopus.files.storage-impls.BackendImplementation]))
 
@@ -14,27 +15,9 @@
   []
   (swap! temp-dir fs/temp-dir "doctopus-temp"))
 
-(defn- load-html-file
-  [rel-path]
-  (let [html-re #"\.html$"
-        assure-suffix #(if (re-find html-re rel-path) rel-path (str rel-path ".html"))
-        file-handle (fs/file @temp-dir rel-path)]
-    (slurp file-handle)))
-
-(defn save-html-file
-  "Expects a key, a relative path, and the data to write. `Key' is
-  assumed to be the name of a particular documentation set, and
-  becomes the primary subdirectory in the filesystem."
-  [key rel-path data]
-  (binding [fs/*cwd* @temp-dir]
-    (let [assure-relativity #(if (re-find #"^/" %) (subs % 1) %)
-          file-handle (fs/file key (assure-relativity rel-path))]
-      (fs/mkdirs (fs/parent file-handle))
-      (spit file-handle data))))
-
 (defn save-fn
   [key path-html-pairs]
-  (doseq [[path html] path-html-pairs] (save-html-file key path html)))
+  (doseq [[path html] path-html-pairs] (save-html-file @temp-dir key path html)))
 
 (defn load-fn
   "This is as Version 1 a way of doing this as I can think of:
