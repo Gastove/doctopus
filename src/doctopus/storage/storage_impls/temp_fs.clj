3ec1c86f62dfb56b7e68bcc7eff01eee6460a269
Ross Donaldson <gastove@gmail.com>
2015-04-22 10:52:21 -0700

    EVERYBODY CHANGE PLACES
    Realized having storage under the file namespace didn't make any dang
    sense :P

--- src/doctopus/storage/storage_impls/temp_fs.clj
+++ src/doctopus/storage/storage_impls/temp_fs.clj
@@ -5,12 +5,12 @@
 
 ;; The root of the temp filesystem. Each Thing will store its stuff
 ;; within this directory
-(def ^:dynamic *temp-dir* (atom (fs/temp-dir "doctopus-temp")))
+(def temp-dir (atom (fs/temp-dir "doctopus-temp")))
 
 (defn regenerate-temp-dir
   "Generates a new temp dir"
   []
-  (swap! *temp-dir* fs/temp-dir "doctopus-temp"))
+  (swap! temp-dir fs/temp-dir "doctopus-temp"))
 
 (defn- load-html-file
   [rel-path]
@@ -19,8 +19,18 @@
         file-handle (fs/file @*temp-dir* rel-path)]
     (slurp file-handle)))
 
+(defn save-html-file
+  "Expects a key, a relative path, and the data to write. `Key' is
+  assumed to be the name of a particular documentation set, and
+  becomes the primary subdirectory in the filesystem."
+  [key rel-path data]
+  (binding [fs/*cwd* @temp-dir]
+    (let [file-handle (fs/file key rel-path)]
+      (spit file-handle data))))
+
 (defn save-fn
-  [])
-;; Reminder of how to do this
-;; (binding [fs/*cwd* @*temp-dir*]
-;;   )
+  [key path-html-pairs]
+  (doseq [[path html] path-html-pairs] (save-html-file key path html)))
+
+(defn load-fn
+  [key])
