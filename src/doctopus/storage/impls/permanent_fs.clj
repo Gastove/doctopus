70ee8d3ad8695a3d8be5428e8ca03a52274a5e35
Ross Donaldson <gastove@gmail.com>
2015-04-23 14:09:03 -0700

    Rework the save function to "save" by copying.

--- src/doctopus/storage/impls/permanent_fs.clj
+++ src/doctopus/storage/impls/permanent_fs.clj
@@ -5,12 +5,20 @@
             [me.raynes.fs :as fs]
             [doctopus.storage.impls.fs-impl :refer [save-html-file] :as fs-impl]))
 
+;; TODO: this does not actually work the way we need it to! Neat! --RMD
+
 (def root (fs/file (:permanent-fs-root (server-config))))
 (if-not (fs/exists? root) (fs/mkdirs root))
 
 (defn save-fn
-  [key path-html-pairs]
-  (doseq [[path html] path-html-pairs] (save-html-file root key path html)))
+  "Save a dir of html stuff by moving it in to the annointed dir in
+  the permanent fs"
+  [key src-dir]
+  (binding [fs/*cwd* root]
+    (let [target-dir (fs/file key)
+          result (fs/copy-dir src-dir target-dir)]
+      (and (fs/exists? result)
+           (fs/readable? result)))))
 
 (defn load-fn
   "This is as Version 1 a way of doing this as I can think of:
