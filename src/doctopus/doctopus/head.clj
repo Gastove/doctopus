4a9ce94a575ae26b921afe913d6050a6c7715ebb
Ross Donaldson <gastove@gmail.com>
2015-04-23 13:43:48 -0700

    Wire up bootstrapping a head

--- src/doctopus/doctopus/head.clj
+++ src/doctopus/doctopus/head.clj
@@ -1,11 +1,23 @@
-(ns doctopus.doctopus.head)
+(ns doctopus.doctopus.head
+  (:require [clojure.edn :as edn]
+            [me.raynes.fs :as fs]
+            [doctopus.doctopus.tentacle :refer [map->Tentacle]]))
 
 
 ;; Head
 (defprotocol HeadMethods
+  (bootstrap-tentacles [this root])
   (list-tentacles [this]))
 
 (defrecord Head
-    [tentacles]
+    [name]
   HeadMethods
+  (bootstrap-tentacles [this root]
+    (let [tentacles-root (binding [fs/*cwd* root] (fs/file (:name this)))
+          tentacle-files (fs/list-dir tentacles-root)
+          tentacle-configs (for [tf tentacle-files
+                                 :let [strang (slurp tf)]]
+                             (edn/read-string strang))
+          tentacles (map #(map->Tentacle %) tentacle-configs)]
+      (assoc this :tentacles tentacles)))
   (list-tentacles [this] (:tentacles this)))
