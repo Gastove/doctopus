9a50e310f5576227b589469ec1f04b90e74572ae
Ross Donaldson <gastove@gmail.com>
2015-04-23 13:44:16 -0700

    Wire up bootstrapping a tentacle from an edn config on disk

--- src/doctopus/doctopus/tentacle.clj
+++ src/doctopus/doctopus/tentacle.clj
@@ -1,10 +1,27 @@
-(ns doctopus.doctopus.tentacle)
+(ns doctopus.doctopus.tentacle
+  (:require [doctopus.storage :refer [save-to-storage load-from-storage backend]]
+            [doctopus.shell :refer [make-html]]))
 
 
 ;; TENTACLES
-(defprotocol TentacleMethods)
+(defprotocol TentacleMethods
+  (generate-html [this])
+  (save-build-output [this]))
 
 (defrecord Tentacle
-    []
+    [name html-command html-args src-root output-root]
   TentacleMethods
-  )
+  (generate-html [this]
+    (let [{:keys [html-command html-args src-root]} this]
+      (apply make-html [html-command html-args src-root])))
+  (save-build-output [this]
+    (let [{:keys [name output-root]} this])
+    (save-to-storage backend name output-root)))
+
+
+;; ### Tentacle Config Spec
+(def one-tentacle (map->Tentacle {:src-root "/tmp/doctopus"
+                                  :output-root "/tmp/doctopus/docs"
+                                  :html-command "lein"
+                                  :html-args ["marg" "-f" "index.html"]
+                                  :name "doctopus"}))
