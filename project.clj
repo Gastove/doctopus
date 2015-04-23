5016c28d046352df7e560e09ef7f2fe56ab8aea9
Ross Donaldson <gastove@gmail.com>
2015-04-22 20:56:03 -0700

    Update this!

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject doctopus "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "An un-opinionated framework for docs on the wobs"
+  :url "http://github.com/Gastove/doctopus"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
