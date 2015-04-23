7aa59ec8fca5d5121cd062f97fc64d13a7a6360e
Ross Donaldson <gastove@gmail.com>
2015-04-23 16:58:12 -0700

    Add the ring mock lib

--- project.clj
+++ project.clj
@@ -14,7 +14,8 @@
                  [ring/ring-core "1.3.2"]
                  [ring/ring-devel "1.3.2"]
                  [bidi "1.18.10"]
-                 [enlive "1.1.5"]]
+                 [enlive "1.1.5"]
+                 [ring/ring-mock "0.2.0"]]
   :plugins [[lein-marginalia "0.8.0"]]
   :main ^:skip-aot doctopus.web
   :target-path "target/%s"
