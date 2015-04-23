244fb9ed3627ba42312fb5879adf2484308843fa
Ross Donaldson <gastove@gmail.com>
2015-04-23 15:10:13 -0700

    Update the permanent fs API to load a file handler

--- src/doctopus/storage/impls/permanent_fs.clj
+++ src/doctopus/storage/impls/permanent_fs.clj
@@ -35,7 +35,7 @@
   [key]
   (let [file-handle (binding [fs/*cwd* root] (fs/file key))
         file-name (.getPath file-handle)]
-   [key (bidi-ring/->Files file-name)]))
+    [key (bidi-ring/->Files {:dir file-name})]))
 
 (defn remove-fn
   [key]
