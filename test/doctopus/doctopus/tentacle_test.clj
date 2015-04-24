bb0ee345c3b4b06f6c6d2a67caf89ce2f68147da
Ross Donaldson <gastove@gmail.com>
2015-04-24 16:18:26 -0700

    WIP

--- test/doctopus/doctopus/tentacle_test.clj
+++ test/doctopus/doctopus/tentacle_test.clj
@@ -1,6 +1,12 @@
 (ns doctopus.doctopus.tentacle-test
-  (:require [clojure.test :refer :all]
+  (:require [clojure.edn :as edn]
+            [clojure.java.io :as io]
+            [clojure.test :refer :all]
             [doctopus.doctopus.tentacle :refer :all]
             [me.raynes.fs :as fs]))
 
-(def one-tentacle)
+(def test-map-props
+  (let [base-map (edn/read-string (io/resource "resources/self/heads/main/doctopus"))]
+    (assoc base-map :name "doctopus-test")))
+
+(def one-tentacle (map->Tentacle ))
