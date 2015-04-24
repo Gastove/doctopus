05ae89caf5e5338c16a6f6821a48eb3a23bc7159
Ross Donaldson <gastove@gmail.com>
2015-04-24 10:28:41 -0700

    Add a sick hack to get the entrypoint from a tentacle

--- src/doctopus/doctopus/tentacle.clj
+++ src/doctopus/doctopus/tentacle.clj
@@ -1,5 +1,6 @@
 (ns doctopus.doctopus.tentacle
-  (:require [doctopus.shell :refer [make-html git-clone]]
+  (:require [clojure.string :as str]
+            [doctopus.shell :refer [make-html git-clone]]
             [doctopus.storage :refer [save-to-storage load-from-storage backend]]
             [me.raynes.fs :as fs]
             [taoensso.timbre :as log]))
@@ -13,10 +14,11 @@
   (load-html [this])
   (generate-html [this])
   (save-build-output [this dir])
+  (get-html-entrypoint [this])
   (routes [this]))
 
 (defrecord Tentacle
-    [name html-command html-args output-root source-location]
+    [name html-command html-args output-root source-location entry-point]
   TentacleMethods
   (load-html [this]
     (if (nil? (load-from-storage backend (:name this)))
@@ -37,6 +39,8 @@
   (save-build-output [this dir]
     (let [{:keys [name]} this])
     (save-to-storage backend name dir))
+  (get-html-entrypoint [this]
+    (str/join "/" ["" "docs" (:name this) (:entry-point this)]))
   (routes [this]
     (load-from-storage backend (:name this))))
 
