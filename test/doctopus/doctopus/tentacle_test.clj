f53e4a873bab010bc2fcbb157d461530ce88aa69
Ross Donaldson <gastove@gmail.com>
2015-04-24 16:10:49 -0700

    Add a tentacle test ns

---
+++ test/doctopus/doctopus/tentacle_test.clj
@@ -0,0 +1,6 @@
+(ns doctopus.doctopus.tentacle-test
+  (:require [clojure.test :refer :all]
+            [doctopus.doctopus.tentacle :refer :all]
+            [me.raynes.fs :as fs]))
+
+(def one-tentacle)
