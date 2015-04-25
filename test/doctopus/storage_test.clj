b0967a6dbc31a17e29af5529e859818267000e64
Ross Donaldson <gastove@gmail.com>
2015-04-25 11:09:11 -0700

    Actual storage tests

--- test/doctopus/storage_test.clj
+++ test/doctopus/storage_test.clj
@@ -1,3 +1,14 @@
 (ns doctopus.storage-test
   (:require [doctopus.storage :refer :all]
             [clojure.test :refer :all]))
+
+
+(deftest storage
+  (testing "Can we swap backends?"
+    (is (= default-backend (get-key-from-backend backend :name))
+        "Are we using the default backend?")
+    (is (set-backend! backend :temp-fs)
+        "Can we set the backend to a differen't implementation?")
+    (is (set-backend! backend default-backend)
+        "And can we set it back to the default"))
+  )
