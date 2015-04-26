6bb28552c302566f20998dfe729b6c3f593d808c
Ross Donaldson <gastove@gmail.com>
2015-04-25 23:45:16 -0700

    Revamp the Doctopus NS docstring

--- src/doctopus/doctopus.clj
+++ src/doctopus/doctopus.clj
@@ -1,19 +1,11 @@
 (ns doctopus.doctopus
-  "Maybe this is an awesome idea? Maybe this is a sub-good idea. We should
-talk about it either way. The idea is: start defining an Entity we can flesh out.
-  Pin properties to. Give more shape.
+  "A Doctopus represents the entire, wriggling, living creature that is your
+  documentation. A Doctopus is a singluar abstraction -- at time of writing, you
+  can only have one. A Doctopus contains all of the configuration necessary for your docs to make it to the web.
 
-  My idea is that a Doctopus represents a *group* of docs. Maybe a collection of
-  repos, or all the docs for a single company, or all the docs for a functional
-  team within a company. Each Doctopus will have a Head -- a centralized notion
-  of what it is and what it does -- and some number of Tentacles -- individual
-  sources of documentation orchestrated by the Head.
-
-  Subject to all kinds of change. For instance: right now I've only really got a
-  gut feeling that a Doctopus needs a Head; I'm not sure I could precisely
-  articulate just _why_ yet.
-
-  I think: Doctopus has several Heads, which each have many Tentacles. I think."
+  Each Doctopus will have one or more Heads, which represent a logical grouping
+  of docs, and some number of Tentacles -- individual sources of documentation
+  orchestrated by the Head. "
   (:require [doctopus.doctopus.head :as h]
             [doctopus.doctopus.tentacle :as t]
             [me.raynes.fs :as fs])
