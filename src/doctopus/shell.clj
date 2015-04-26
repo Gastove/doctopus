825d6adc864ad0d17849fcf96ba9c67dfa101383
Ross Donaldson <gastove@gmail.com>
2015-04-26 14:03:11 -0700

    Adds a function that correctly splits a vector of strings, preserving order, and tests to prove such.

--- src/doctopus/shell.clj
+++ src/doctopus/shell.clj
@@ -1,26 +1,46 @@
 (ns doctopus.shell
-    (:require [clojure.java.shell :as sh]))
+  (:require [clojure.java.shell :as sh]
+            [clojure.string :as str]))
 
 (defn make-html
-"This is a function that we use to shell out to a configured command
-to build the HTML for a given project.
+  "This is a function that we use to shell out to a configured command
+  to build the HTML for a given project.
 
-e.g. cd /to/dir; make html
+  e.g. cd /to/dir; make html
 
-Args:
-    command: string, something that is within the $PATH of a shell.
-    args: a list of strings.
-    root-dir: string, path to the directory where the ``command`` should be run.
-"
-    ([command]
-        (sh/sh command))
-    ([command args]
-        (apply sh/sh (cons command args)))
-    ([command args root-dir]
-        (apply sh/sh (cons command (conj args :dir root-dir)))))
+  Args:
+  command: string, something that is within the $PATH of a shell.
+  args: a list of strings.
+    root-dir: string, path to the directory where the ``command`` should be run. "
+  ([command]
+   (sh/sh command))
+  ([command args]
+   (apply sh/sh (cons command args)))
+  ([command args root-dir]
+   (apply sh/sh (cons command (conj args :dir root-dir)))))
 
 (defn git-clone
   [repo dest]
   (let [res (sh/sh "git" "clone" repo dest)
         status (:exit res)]
     (= 0 status)))
+
+(defn split
+  [string]
+  (str/split string #" "))
+
+(defn injest-shell-vector
+  "Takes a vector of shell commands, like:
+
+  [\"npm i\"
+  \"node bin/mop.js\"]
+
+  And returns a vector of vectors, like:
+
+  [[\"npm\" \"i\"]
+  [\"node\" \"bin/mop.js\"]]
+
+  Note that to work, this _must_ preserve command order in all regards!"
+  [incoming-vec]
+  (->> incoming-vec
+       (map split)))
--- test/doctopus/shell_test.clj
+++ test/doctopus/shell_test.clj
@@ -1,19 +1,33 @@
 (ns doctopus.shell-test
-    (:require [doctopus.shell :refer :all]
-              [clojure.test :refer :all]))
+  (:require [doctopus.shell :refer :all]
+            [clojure.test :refer :all]))
 
-(deftest test-command-no-args
-    (testing "Can we run a command that takes no arguments?"
-        (let [expected "\n"
-              result (make-html "echo")]
-            (is (= (:out result) expected))))
+(deftest make-html-test
+  (testing "Can we run a command that takes no arguments?"
+    (let [expected "\n"
+          result (make-html "echo")]
+      (is (= (:out result) expected))))
 
-    (testing "Can we run a command that takes arguments, but specifies no dir?"
-        (let [expected "biff baz\n"
-              result (make-html "echo" ["biff" "baz"])]
-            (is (= (:out result) expected))))
+  (testing "Can we run a command that takes arguments, but specifies no dir?"
+    (let [expected "biff baz\n"
+          result (make-html "echo" ["biff" "baz"])]
+      (is (= (:out result) expected))))
 
-    (testing "Can we supply a command, args, and a directory to work from?"
-        (let [expected "biff boom baz\n"
-              result (make-html "echo" ["biff" "boom" "baz"] "/")]
-            (is (= (:out result) expected)))))
+  (testing "Can we supply a command, args, and a directory to work from?"
+    (let [expected "biff boom baz\n"
+          result (make-html "echo" ["biff" "boom" "baz"] "/")]
+      (is (= (:out result) expected)))))
+
+
+(deftest injest-shell-vector-test
+  (let [input-string "make -C docs/ html"
+        input-vector ["npm i"
+                       "node bin/mop.js"]
+        single-vec-expected ["make" "-C" "docs/" "html"]
+        vec-o-vecs-expected [["npm" "i"]
+                             ["node" "bin/mop.js"]]]
+    (testing "Can we correctly split strings?"
+      (is (= (split input-string) single-vec-expected)
+          "Can we split a single vector?")
+      (is (= vec-o-vecs-expected (injest-shell-vector input-vector))
+          "When we injest a vector, does it split correctly *and* preserve order?"))))
