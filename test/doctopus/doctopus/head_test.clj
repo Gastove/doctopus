e9b698516aa94ff46aba7ab1f2336592bab64bb7
Ross Donaldson <gastove@gmail.com>
2015-04-25 17:05:35 -0700

    Add a head_test file

---
+++ test/doctopus/doctopus/head_test.clj
@@ -0,0 +1,8 @@
+(ns doctopus.doctopus.head-test
+  (:require [doctopus.doctopus.head :refer :all]
+            [clojure.test :refer :all]))
+
+(def test-head (Head. "doctopus"))
+
+(deftest head-test
+  )
