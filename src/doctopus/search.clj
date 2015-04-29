6de7b9b9ba6503363d479c6aa161ec29a6a8c8ac
Ross Donaldson <gastove@gmail.com>
2015-04-28 21:34:04 -0700

    Start fleshing out a search config

--- src/doctopus/search.clj
+++ src/doctopus/search.clj
@@ -1,4 +1,12 @@
 (ns doctopus.search)
 
-;; TODO:
-;; Figure out an analyzer :P
+(def elastic-search-configs
+  {:settings
+   {:index
+    {"analysis"
+     {"analyzer"
+      {"html_stopwords" {"type" "standard" ;; Cannot find any record of what else might go here
+                         "filter" ["html_strip" "stop"]} ;; Removes English stop words and HTML characters
+       }}}}
+   :mappings {"document" {:properties {:body {:type     "string"
+                                              :analyzer "html_stopwords"}}}}}) ;; Hrm.
