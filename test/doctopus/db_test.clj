7c6a56509e4c159dc68063c0ee0b1dd654900551
Ross Donaldson <gastove@gmail.com>
2015-04-29 22:25:02 -0700

    Propagate rename

--- test/doctopus/db_test.clj
+++ test/doctopus/db_test.clj
@@ -1,25 +1,25 @@
 (ns doctopus.db-test
-  (:require [doctopus.db.core :refer :all]
+  (:require [doctopus.db :refer :all]
             [clojure.test :refer :all]))
 
 (deftest db
   (testing "->kebab-keys transforms all keys to kebab-case"
-    (is (= (#'doctopus.db.core/->kebab-keys {:key_name "lol"}) {:key-name "lol"}))
-    (is (= (#'doctopus.db.core/->kebab-keys {:key_name "lol" :another_key "heh"})
+    (is (= (#'doctopus.db/->kebab-keys {:key_name "lol"}) {:key-name "lol"}))
+    (is (= (#'doctopus.db/->kebab-keys {:key_name "lol" :another_key "heh"})
            {:key-name "lol" :another-key "heh"}))
-    (is (= (#'doctopus.db.core/->kebab-keys
+    (is (= (#'doctopus.db/->kebab-keys
             {:key_name "lol" :another_key "heh" :untouched-key "laffo"})
            {:key-name "lol" :another-key "heh" :untouched-key "laffo"})))
   (testing "->snake-keys transforms all keys to snake-case"
-    (is (= (#'doctopus.db.core/->snake-keys {:key-name "lol"}) {:key_name "lol"}))
-    (is (= (#'doctopus.db.core/->snake-keys {:key-name "lol" :another-key "heh"})
+    (is (= (#'doctopus.db/->snake-keys {:key-name "lol"}) {:key_name "lol"}))
+    (is (= (#'doctopus.db/->snake-keys {:key-name "lol" :another-key "heh"})
            {:key_name "lol" :another_key "heh"}))
-    (is (= (#'doctopus.db.core/->snake-keys
+    (is (= (#'doctopus.db/->snake-keys
             {:key-name "lol" :another-key "heh" :untouched_key "laffo"})
            {:key_name "lol" :another_key "heh" :untouched_key "laffo"})))
   (testing "add-date-fields adds updated and created fields"
-    (is (= (#'doctopus.db.core/add-date-fields {} "right-now")
+    (is (= (#'doctopus.db/add-date-fields {} "right-now")
            {:updated "right-now" :created "right-now"})))
   (testing "add-date-fields does not updated created if present"
-    (is (= (#'doctopus.db.core/add-date-fields {:created "earlier"} "right-now")
+    (is (= (#'doctopus.db/add-date-fields {:created "earlier"} "right-now")
            {:updated "right-now" :created "earlier"}))))
