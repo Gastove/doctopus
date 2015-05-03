80947d885e27c5af8adb352ea46a450426b4a3d4
Ross Donaldson <gastove@gmail.com>
2015-05-03 15:56:46 -0700

    Start unit testing our CRUD layer :D
    Add a testing fixture to set the correct database, bootstrap it,  and perform a teardown
    at the end.

--- test/doctopus/db_test.clj
+++ test/doctopus/db_test.clj
@@ -1,9 +1,31 @@
 (ns doctopus.db-test
-  (:require [doctopus.db :refer :all]
+  (:require [clojure.test :refer :all]
+            [clojure.java.jdbc :as jdbc]
+            [doctopus.configuration :refer [server-config]]
+            [doctopus.db :refer :all]
             [doctopus.db.schema :refer :all]
-            [clojure.test :refer :all]))
+            [doctopus.test-utilities :as utils]
+            [korma.core :as sql]
+            [korma.db :refer [defdb postgres]]))
 
-(deftest db
+(defdb test-db (postgres
+                (select-keys (get-in (server-config) [:database :test])
+                             [:db :user :password :host :port])))
+
+(defn- truncate!
+  [table-name]
+  (let [sql-string (format "TRUNCATE %s CASCADE" table-name)]
+    (do-sql-with-logging! sql-string :test)))
+
+(defn database-fixture
+  [f]
+  (bootstrap :test)
+  (f)
+  (doseq [n (keys table-name->schema)] (truncate! n)))
+
+(use-fixtures :once database-fixture)
+
+(deftest db-name-and-data-transforms
   (testing "->kebab-keys transforms all keys to kebab-case"
     (is (= (#'doctopus.db/->kebab-keys {:key_name "lol"}) {:key-name "lol"}))
     (is (= (#'doctopus.db/->kebab-keys {:key_name "lol" :another_key "heh"})
@@ -31,3 +53,22 @@
            "//meow:5432/cats"))
     (is (= (#'doctopus.db.schema/get-subname {:host "meow" :db "cats" :port 31})
            "//meow:31/cats"))))
+
+(deftest db-joins-and-lookups
+  (let [tentacle-mocker #(utils/mock-data :tentacle nil)
+        mock-tentacles (take 5 (repeatedly tentacle-mocker))
+        head-mocker #(utils/mock-data :head nil)
+        mock-heads (take 5 (repeatedly head-mocker))]
+    (testing "Can we save heads?"
+      (doseq [head mock-heads] (save-head! head))
+      (is (= 5 (count (get-all-heads)))))
+    (testing "Can we save tentacles?"
+      (doseq [tentacle mock-tentacles] (save-tentacle! tentacle))
+      (is (= 5 (count (get-all-tentacles)))))
+    (testing "Can we save head-tentacle mappings?"
+      (doall (map #(create-mapping! % %2) mock-heads mock-tentacles))
+      (is (= 5 (count (get-all-mappings)))))
+    (doseq [head mock-heads
+            tentacle mock-tentacles]
+      (delete-head! head)
+      (delete-tentacle! tentacle))))
