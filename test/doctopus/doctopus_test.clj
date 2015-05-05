da98af5d0b55802f5a1c160c0af4856f24bfd922
Ross Donaldson <gastove@gmail.com>
2015-05-05 14:50:14 -0700

    Fix database tests

--- test/doctopus/doctopus_test.clj
+++ test/doctopus/doctopus_test.clj
@@ -13,34 +13,24 @@
            [doctopus.doctopus.head Head]))
 
 (use-fixtures :once database-fixture)
+
 (storage/set-backend! :temp-fs)
 
-(def doctopus-test-configs
-  {:heads-dir (io/resource "test/heads")})
-
-(def test-tent-props
-  (edn/read-string (slurp (io/resource "test/heads/test/doctopus-test.edn"))))
-
-(def stunt-doctopus (Doctopus. doctopus-test-configs {}))
+(def stunt-doctopus (Doctopus. {} {}))
 
 (deftest doctopus-test
-  (let [test-head (Head. "test")
-        test-tent (t/map->Tentacle test-tent-props)]
-    (db/save-head! test-head)
-    (db/save-tentacle! test-tent)
-    (db/create-mapping! test-head test-tent)
-    (testing "Can we list heads?"
-      (is (not (empty? (list-heads stunt-doctopus))))
-      (is (= "test" (:name (first (list-heads stunt-doctopus))))))
-    (testing "Can we list tentacles?"
-      (let [tent-list (list-tentacles stunt-doctopus)]
-        (is (= 1 (count tent-list))
-            "We should only have one of these")
-        (is (= "doctopus-test" (:name (first tent-list)))
-            "Would sure be weird if we got a different tentacle here")))
-    (testing "Can we get a tentacle by its head?"
-      (is (= 1 (count (list-tentacles-by-head stunt-doctopus "test")))
-          "This should get one tentacle")
-      (is (empty? (list-tentacles-by-head stunt-doctopus "HOOBASTANK"))
-          "There should never, ever be a tentacle with this name. Ever.")))
-  (utils/clean-up-test-html "doctopus-test"))
+  (testing "Can we list heads?"
+    (is (not (empty? (list-heads stunt-doctopus))))
+    (is (= "main" (:name (first (list-heads stunt-doctopus))))))
+  (testing "Can we list tentacles?"
+    (let [tent-list (list-tentacles stunt-doctopus)]
+      (is (= 1 (count tent-list))
+          "We should only have one of these")
+      (is (= "doctopus" (:name (first tent-list)))
+          "Would sure be weird if we got a different tentacle here")))
+  (testing "Can we get a tentacle by its head?"
+    (is (= 1 (count (list-tentacles-by-head stunt-doctopus "main")))
+        "This should get one tentacle")
+    (is (empty? (list-tentacles-by-head stunt-doctopus "HOOBASTANK"))
+        "There should never, ever be a tentacle with this name. Ever."))
+  (utils/clean-up-test-html "doctopus"))
