937124853347eda8c1f3a1b083663b846e6b0ec4
Ross Donaldson <gastove@gmail.com>
2015-05-08 16:28:28 -0700

    Why do we even still have these?

--- test/doctopus/core_test.clj
+++
@@ -1,5 +0,0 @@
-(ns doctopus.core-test
-  (:require [clojure.test :refer :all]
-            [doctopus.core :refer :all]))
-
-;; I'm leaving this NS for now, but at the moment there's nothing real to test.
