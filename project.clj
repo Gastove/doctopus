fd3f1a424e190f0932a4e586fc185d66d1c5a552
jesse keane <me@jessekeane.me>
2015-05-25 18:37:57 -0700

    !noidea

--- project.clj
+++ project.clj
@@ -3,29 +3,30 @@
   :url "http://github.com/Gastove/doctopus"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-beta2"]
+  :dependencies [[org.clojure/clojure "1.7.0-beta3"]
                  [joda-time/joda-time "2.6"]
                  [markdown-clj "0.9.63"]
                  [me.raynes/fs "1.4.6"]
                  [http-kit "2.1.19"]
                  [jarohen/nomad "0.7.0"]
-                 [com.taoensso/timbre "3.4.0"]
+                 [com.taoensso/timbre "3.4.0" :exclusions [org.clojure/tools.reader]]
                  [ring/ring-defaults "0.1.4"]
                  [ring/ring-core "1.3.2"]
                  [ring/ring-devel "1.3.2"]
-                 [bidi "1.18.10"]
+                 [bidi "1.19.0" :exclusions [org.clojure/tools.reader]]
                  [enlive "1.1.5"]
                  [ring/ring-mock "0.2.0"]
                  [korma "0.4.0"]
                  [clj-time "0.9.0"]
                  [postgresql/postgresql "8.4-702.jdbc4"]
                  [camel-snake-kebab "0.3.1" :exclusions [org.clojure/clojure]]
-                 [org.clojure/clojurescript "0.0-3269"]
-                 [reagent "0.5.0"]]
+                 [org.clojure/clojurescript "0.0-3269" :exclusions [org.clojure/tools.reader]]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [reagent "0.5.0" :exclusions [org.clojure/tools.reader]]]
   :plugins [[lein-marginalia "0.8.0"] [lein-cljsbuild "1.0.6"]]
   :main ^:skip-aot doctopus.web
   :target-path "target/%s"
-  :cljsbuild {:builds [{:source-paths ["src-cljs/doctopus"]
+  :cljsbuild {:builds [{:source-paths ["src-cljs"]
                         :compiler {:output-to "resources/public/assets/scripts/main.js"
                                    :optimizations :none
                                    :pretty-print true}}]}
