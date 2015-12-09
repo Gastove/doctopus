21a11f9be6524ebac0d1564108ac8305b401b121
Ross Donaldson <gastove@gmail.com>
2015-12-08 16:46:27 -0800

    Primary re-wiring/finishing of the postgres backend

--- src/doctopus/storage/impls/postgres_impl.clj
+++ src/doctopus/storage/impls/postgres_impl.clj
@@ -3,13 +3,19 @@
             [doctopus.files :as f])
   (:import [java.nio.file Files Paths]))
 
-(def is-img-regex
-  #"\.(png|pdf|jpg|jpeg|gif|ico|){1}$")
+(def is-img-regex #"^image")
 
 (defn get-mime-type
-  [path-str]
-  (let [path (Paths/get path-str (into-array String []))]
+  [f]
+  (let [path (.toPath f)]
     (Files/probeContentType path)))
+
+(defn assign-body-and-image
+  [base-doc doc-path mime-type]
+  (let [doc-content (slurp doc-path)]
+    (if (re-find is-img-regex mime-type)
+      (assoc base-doc :body :image :image (.getBytes doc-content))
+      (assoc base-doc :body doc-content :image nil))))
 
 (defn save-fn
   "Load every document from a directory in to the database.
@@ -24,13 +30,14 @@
   (let [relpath-file-pairs (f/list-files-with-relative-paths src-dir)
         tent-name (:name tentacle)]
     (doseq [[relpath doc-file] relpath-file-pairs
-            :let [new-doc {:name (str tent-name "-" (.getName doc-file))
-                           :body (slurp doc-file)
-                           :path doc-file
-                           :type (get-mime-type doc-file)
-                           :uri (str tent-name "/" relpath)
-                           :tentacle-name tent-name}]]
-      (db/save-document! new-doc))
+            :let [doc-mime-type (get-mime-type doc-file)
+                  base-doc {:name (str tent-name "-" (.getName doc-file))
+                            ;; :path doc-file
+                            :mime-type doc-mime-type
+                            :uri (str tent-name "/" relpath)
+                            :tentacle-name tent-name}
+                  new-doc (assign-body-and-image base-doc doc-file doc-mime-type)]]
+      (db/save-document! new-doc false))
     (db/update-document-index)))
 
 (defn load-fn
