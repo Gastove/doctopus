5d8d7cd2984be6cf12320c33f553dc041ee10ade
fardog <nate@milkandtang.com>
2015-12-09 15:12:54 -0800

    open/close omnibar with home link

--- src-cljs/omni.cljs
+++ src-cljs/omni.cljs
@@ -1,15 +1,50 @@
 (ns doctopus.omni
   (:require [goog.dom :as dom]
-            [reagent.core :as reagent]))
+            [reagent.core :as r]))
+
+(enable-console-print!)
+
+(defonce state (r/atom {:open false}))
+
+(defn- open-close-omnibar
+  [event]
+  (.stopPropagation event)
+  (.preventDefault event)
+  (swap! state assoc :open (not (:open @state)))
+  (println @state))
+
+(defn- brand-component
+  []
+  [:div#brand
+     [:a {:href "#"
+          :on-click open-close-omnibar} "doctopus"]])
+
+(defn- close-component
+  []
+  [:div.icon.close
+   [:a {:href "#"
+          :on-click open-close-omnibar} "close"]])
+
+(defn- logo-component
+  []
+  [:div.logo [:a {:href "/"} "doctopus"]])
+
+(defn- sidebar-component
+  []
+  [:div#sidebar
+   [logo-component]
+   [close-component]])
+
 
 (defn omnibar
   []
-  (fn []
-    [:div#brand
-     [:a {:href "/"} "doctopus"]]))
+  (let [{:keys [open]} @state]
+    (if open
+      [sidebar-component]
+      [brand-component])))
 
 (defn init
   [component]
-  (reagent/render-component component (dom/getElement "doctopus-omnibar")))
+  (r/render-component component (dom/getElement "doctopus-omnibar")))
 
-(init (omnibar))
+(init [omnibar])
