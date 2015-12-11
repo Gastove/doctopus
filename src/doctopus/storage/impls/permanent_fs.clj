fa28858a67e83525fd7451b91a50fed150f6aad5
Ross Donaldson <gastove@gmail.com>
2015-12-11 11:30:03 -0800

    Get this thing back in to shape
    Needs to change to writing things in to
    root/`docs-uri-prefix`/tentacle-name to support the URI scheme

--- src/doctopus/storage/impls/permanent_fs.clj
+++ src/doctopus/storage/impls/permanent_fs.clj
@@ -2,11 +2,12 @@
   (:require [clojure.string :as str]
             [doctopus.configuration :refer [docs-uri-prefix]]
             [doctopus
-             [configuration :refer [server-config]]
+             [configuration :refer [server-config docs-uri-prefix]]
              [files :as files]]
             [doctopus.storage.impls.fs-impl :refer [save-html-file] :as fs-impl]
             [me.raynes.fs :as fs]
-            [ring.util.response :refer [file-response]]
+            [ring.util.mime-type :refer [ext-mime-type]]
+            [ring.util.response :as response]
             [taoensso.timbre :as log]))
 
 (def root (fs/file (:permanent-fs-root (server-config))))
@@ -19,27 +20,36 @@
 
 (if-not (fs/exists? root) (fs/mkdirs root))
 
-;; TODO:
-(defn count-fn [] nil)
+(defn count-fn [tentacle]
+  (binding [fs/*cwd* (fs/file root docs-uri-prefix)]
+    (let [target-dir (fs/file (:name tentacle))]
+      (count (files/walk-docs-dir target-dir)))))
 
 (defn save-fn
   "Save a dir of html stuff by moving it in to the annointed dir in
   the permanent fs"
   [key src-dir]
-  (binding [fs/*cwd* root]
+  (binding [fs/*cwd* (fs/file root docs-uri-prefix)]
     (let [target-dir (fs/file key)
           result (fs/copy-dir src-dir target-dir)]
       (and (fs/exists? result)
            (fs/readable? result)))))
 
+(defn- build-response
+  [file-handle]
+  (let [body (slurp file-handle)
+        file-name (.getPath file-handle)
+        mime-type (ext-mime-type file-name)]
+    (-> (response/response body)
+        (response/content-type mime-type))))
+
 (defn load-fn
   "Returns a file from storage if we have one, otherwise nil"
   [uri]
-  (let [rel-path (str/replace uri (str docs-uri-prefix "/") "") ;; Remove URI prefix to get relative path
-        file-handle (binding [fs/*cwd* root] (fs/file rel-path))
-        file-name (.getPath file-handle)]
+  (let [rel-path (str/replace-first uri  "/" "") ;; Remove the leading slash so we can treat as a relative path
+        file-handle (binding [fs/*cwd* root] (fs/file rel-path))]
     (if (fs/exists? file-handle)
-      (file-response file-name)
+      (build-response file-handle)
       nil)))
 
 (defn remove-fn
