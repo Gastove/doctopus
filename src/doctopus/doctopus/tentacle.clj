f5d0e8cf526832b1c4c07fcfbd0835ecb72e9dfb
Ross Donaldson <gastove@gmail.com>
2015-12-12 20:23:50 -0800

    Cleanup, documentation, docstrings

--- src/doctopus/doctopus/tentacle.clj
+++ src/doctopus/doctopus/tentacle.clj
@@ -16,20 +16,12 @@
   (:import java.io.ByteArrayInputStream))
 
 ;; To match against mimetype strings
-(def is-img-regex #"^image")
+(def image-re #"^image")
 
-;; Utilities for marking HTTP Resposnes
-(defn- assign-body-and-image
-  [base-doc doc-file mime-type]
-  (log/debug base-doc)
-  (let [doc-content (slurp doc-file)]
-    (if (re-find is-img-regex mime-type)
-      (assoc base-doc :body "image" :image (.getBytes doc-content))
-      (assoc base-doc :body doc-content :image nil))))
-
+;; ### Utilities for marking HTTP Resposnes
 (defn- build-body
-  "Examine a DB result to see if it's an image; if so, build a ByteArrayInputStream
-  and use it as the response body."
+  "Examine a DB result to see if it's an image; if so, build a
+  ByteArrayInputStream and use it as the response body."
   [res]
   (if (= "image" (:body res))
     (let [bais (ByteArrayInputStream. (:image res))]
@@ -37,11 +29,14 @@
     res))
 
 (defn- make-response
+  "Creates an HTTP response from a db result and set its mime type"
   [res]
   (-> (response/response (:body res))
       (response/content-type (:mime-type res))))
 
 (defn- load-fn
+  "If a db result is found for a URI, create an HTTP response; otherwise return
+  nil"
   [uri]
   (if-let [res (first (db/get-document-by-uri uri))]
     (-> res
@@ -56,24 +51,41 @@
   (git-clone sc-location dest))
 
 ;; Storing generated HTML
+(defn- assign-body-and-image
+  "Takes a map representing a new document, a file handle, and a mime type; if
+  the file is determined to be an image, give the document the body \"image\"
+  and load that images byte array in to the :image field. Otherwise, the :body
+  key just gets the documents content and image is nil."
+  [base-doc doc-file mime-type]
+  (log/debug base-doc)
+  (let [doc-content (slurp doc-file)]
+    (if (re-find image-re mime-type)
+      (assoc base-doc :body "image" :image (.getBytes doc-content))
+      (assoc base-doc :body doc-content :image nil))))
+
 (defn- save-to-database
-  "Load every document from a directory in to the database.
+  "Takes the name of a tentacle and the directory that tentacle's HTML is in.
+  Walks the html dir, loading every document found in to the database.
 
-  Note: at time of writing, at the end of load the call to update-document-index
-  will recalculate the _entire_ `search_vector' column in PG. If this ever gets
-  too costly, it can be improved, probably by adding additional info to the
-  update's `where' clause.
+  Note: at time of writing, at the end of load the call to
+  update-document-index will recalculate the _entire_ `search_vector'
+  column in PG. If this ever gets too costly, it can be improved,
+  probably by adding additional info to the update's `where' clause.
 
-  Also note: there are two kinds of files to be loaded: text and images (binary)."
+  Also note: there are two kinds of files to be loaded: text and
+  images (binary). Forming correct document bodies is handled by
+  `assign-body-and-image'."
   [tent-name src-dir]
   (let [relpath-file-pairs (f/list-files-with-relative-paths src-dir)]
     (doseq [[relpath doc-file] relpath-file-pairs
             :let [doc-mime-type (ring-mime/ext-mime-type relpath)
                   base-doc {:name (str tent-name "-" (.getName doc-file))
-                            ;; :path doc-file
                             :mime-type doc-mime-type
+                            ;; Pre-bake a URI, for direct routing later.
                             :uri (str "/" configuration/docs-uri-prefix "/" tent-name relpath)
                             :tentacle-name tent-name}]]
+      ;; Sphinx in particular uses obtuse binary format files to contain object
+      ;; mapping data. For now, we refuse to load them in all together.
       (if (nil? doc-mime-type)
         (log/warn "Refusing to save" relpath ", cannot discern mime-type")
         (let [new-doc (assign-body-and-image base-doc doc-file doc-mime-type)]
@@ -131,6 +143,7 @@
       (if success?
         (do (binding [fs/*cwd* target-dir]
               (let [html-dir (fs/file output-root)]
+                (log/debug html-dir)
                 (make-html-from-vec html-commands target-dir)
                 (check-and-report
                  (save-build-output this html-dir) name "generated HTML" "generate HTML"))))
